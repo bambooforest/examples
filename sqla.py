c7ea75f2383a6234ba8443ded103a71719b78247
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-04-03 18:00:53 +0200

    improve parent/child sqlalchemy example

--- sqla.py
+++ sqla.py
@@ -1,34 +1,45 @@
+# sqla.py - insert into a parent and child table
+
 import sqlalchemy as sa
 import sqlalchemy.orm
 import sqlalchemy.ext.declarative
 
-engine = sa.create_engine('sqlite:///example.sqlite3')
+engine = sa.create_engine('sqlite://', echo=True)
+
 
 Base = sa.ext.declarative.declarative_base()
+
 
 class Parent(Base):
 
      __tablename__ = 'parent'
 
      id = sa.Column(sa.Integer, primary_key=True)
-     name = sa.Column(sa.Text, nullable=False)
+
+     name = sa.Column(sa.Text, unique=True, nullable=False)
+
 
 class Child(Base):
 
      __tablename__ = 'child'
 
      id = sa.Column(sa.Integer, primary_key=True)
-     name = sa.Column(sa.Text, nullable=False)
+
      parent_id = sa.Column(sa.ForeignKey('parent.id'), nullable=False)
+     name = sa.Column(sa.Text, nullable=False)
+
+     __table_args__ = (sa.UniqueConstraint(parent_id, name),)
 
 
 Base.metadata.create_all(engine)
 
-items = [('spam', ('eggs', 'bacon'))]
+
+items = [('spam', ('eggs', 'bacon')), ('ham', ('spam', 'eggs'))]
 
 with engine.begin() as conn:
+     insert_parent = sa.insert(Parent, bind=conn).execute
+     insert_child = sa.insert(Child, bind=conn).execute
      for parent, children in items:
-         p_id, = sa.insert(Parent,
-bind=conn).execute(name=parent).inserted_primary_key
+         p_id, = insert_parent(name=parent).inserted_primary_key
          params = [{'name': c, 'parent_id': p_id} for c in children]
-         sa.insert(Child, bind=conn).execute(params)
+         insert_child(params)
